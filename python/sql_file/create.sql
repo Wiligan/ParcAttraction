b256aba21a8eeb8a226ac6c184926c780e3c9cb8
Adrien Aubert <adrien.aubert9@etu.univ-lorraine.fr>
2024-02-02 16:15:02 +0100

    Modifie la visibilité des attractions pour les utilisateurs

--- python/sql_file/create.sql
+++ python/sql_file/create.sql
@@ -1,4 +1,6 @@
 INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Silver Star', 'Montagne russe', 3, 1);
 INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Montagne 8', 'Montagne russe', 4, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Attraction visible', 'Montagne russe', 3, 1);
+INSERT INTO attraction (nom, description, difficulte, visible) VALUES ('Attraction non visible', 'Montagne russe', 4, 0);
 
 INSERT INTO users (name, password) VALUES ('toto', 'toto');
