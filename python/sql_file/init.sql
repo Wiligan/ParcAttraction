85b915d314c87f1e9c235dae2a206eec5b2ee72f
Adrien Aubert <adrien.aubert9@etu.univ-lorraine.fr>
2024-02-02 17:21:54 +0100

    Modifie la syntaxe des fichiers SQL

--- python/sql_file/init.sql
+++ python/sql_file/init.sql
@@ -2,8 +2,7 @@ DROP TABLE IF EXISTS attraction;
 
 CREATE TABLE attraction
 (
-    attraction_id int auto_increment,
-    primary key (attraction_id),
+    attraction_id int auto_increment PRIMARY KEY,
     nom           varchar(255) not null,
     description   varchar(255) not null,
     difficulte    int,
@@ -25,12 +24,11 @@ DROP TABLE IF EXISTS critique;
 
 CREATE TABLE critique
 (
-    critique_id   int auto_increment,
-    primary key (critique_id),
+    critique_id   int auto_increment PRIMARY KEY,
     prenom        varchar(255) null,
     nom           varchar(255) null,
     critique      varchar(255) not null,
-    note INT not null,
-    attraction_id INT not null,
-    CONSTRAINT fk_id_attraction FOREIGN KEY (attraction_id) REFERENCES attraction (attraction_id)
+    note          INT          not null,
+    attraction_id INT          not null,
+    CONSTRAINT fk_attraction_id FOREIGN KEY (attraction_id) REFERENCES attraction (attraction_id)
 );
