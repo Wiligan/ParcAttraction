fc3462258f14cfa574b415de20a0bafcb914fe41
Adrien Aubert <adrien.aubert9@etu.univ-lorraine.fr>
2024-02-02 16:22:42 +0100

    Reformate le sql

--- python/sql_file/init.sql
+++ python/sql_file/init.sql
@@ -1,32 +1,35 @@
 DROP TABLE IF EXISTS attraction;
 
-CREATE TABLE attraction (
+CREATE TABLE attraction
+(
     attraction_id int auto_increment,
-    primary key(attraction_id),
-    nom varchar(255) not null,
-    description varchar(255) not null,
-    difficulte int,
-    visible bool default true
+    primary key (attraction_id),
+    nom           varchar(255) not null,
+    description   varchar(255) not null,
+    difficulte    int,
+    visible       bool default true
 );
 
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
+CREATE TABLE users
+(
     users_id int auto_increment,
-    primary key(users_id),
-    name varchar(255) not null,
+    primary key (users_id),
+    name     varchar(255) not null,
     password varchar(255) not null
 );
 
 
 DROP TABLE IF EXISTS critiques;
 
-CREATE TABLE critiques (
-    critique_id int auto_increment,
-    primary key(critique_id),
-    prenom varchar(255) not null,
-    nom varchar(255) not null,
-    critique varchar(255) not null,
+CREATE TABLE critiques
+(
+    critique_id   int auto_increment,
+    primary key (critique_id),
+    prenom        varchar(255) not null,
+    nom           varchar(255) not null,
+    critique      varchar(255) not null,
     attraction_id INT,
-    CONSTRAINT fk_id_attraction FOREIGN KEY (attraction_id) REFERENCES attraction(attraction_id)
+    CONSTRAINT fk_id_attraction FOREIGN KEY (attraction_id) REFERENCES attraction (attraction_id)
 );
